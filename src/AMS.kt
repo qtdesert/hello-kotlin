95f441f3bd5bd3105863cc6a633a8bd6f1b2426b
Quentin Désert <qtdesert@gmail.com>
2020-05-29 18:16:30 -0500

    Changed water

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,19 +1,42 @@
-import java.util.*
+import java.util.Random
 
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}!")
+    feedTheFish()
 }
 
-fun dayOfWeek() {
-    println("What day is today?")
-    println(when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-        1 -> "Sunday"
-        2 -> "Monday"
-        3 -> "Tuesday"
-        4 -> "Wednesday"
-        5 -> "Thursday"
-        6 -> "Friday"
-        7 -> "Saturday"
-        else -> "Time has stopped"
-    })
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20) : Boolean {
+    return true
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+
+    swim(50, speed="slow")
+
+    if (shouldChangeWater(day)) {
+        println("Change the water today")
+    }
+}
+
+fun swim(time: Int, speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
 }
