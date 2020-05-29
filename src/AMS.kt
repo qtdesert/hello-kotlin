7217717643d9dadec030a0df0dbefbf4ae1649d7
Quentin Désert <qtdesert@gmail.com>
2020-05-29 14:45:39 -0500

    AMS project files

---
+++ src/AMS.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    dayOfWeek()
+}
+
+fun dayOfWeek() {
+    println("What day is today?")
+    println(when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
+        else -> "Time has stopped"
+    })
+}
