cb049476832c123dc1edb2b9e42566f8c3def370
Quentin Désert <quentin.desert@gmail.com>
2020-05-31 18:33:35 -0500

    Aquarium class

---
+++ src/aquarium/main.kt
@@ -0,0 +1,19 @@
+package aquarium
+
+fun main(args: Array<String>) {
+    buildAquarium()
+}
+
+private fun buildAquarium() {
+    val myAquarium = Aquarium()
+
+    println("Length: ${myAquarium.length} " +
+            "Width: ${myAquarium.width} " +
+            "Height: ${myAquarium.height}")
+
+    myAquarium.height = 80
+
+    println("Height: ${myAquarium.height} cm")
+
+    println("Volume: ${myAquarium.volume} liters")
+}
