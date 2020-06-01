5107db4d8c793846fefea710e1777d9af28a11e9
Quentin Désert <quentin.desert@gmail.com>
2020-06-01 12:44:47 -0500

    Removed unused code

--- src/spices/Spice.kt
+++ src/spices/Spice.kt
@@ -5,49 +5,13 @@ fun main(args: Array<String>) {
 }
 
 fun delegate() {
-//    val mySimpleSpice = SimpleSpice()
-//    println("Name: ${mySimpleSpice.name}")
-//    println("Spiciness: ${mySimpleSpice.spiciness}")
-//    println("Heat: ${mySimpleSpice.heat}")
-//
-//    println("Setting heat to 9")
-//    mySimpleSpice.heat = 9
-//    println("Name: ${mySimpleSpice.name}")
-//    println("Spiciness: ${mySimpleSpice.spiciness}")
-//    println("Heat: ${mySimpleSpice.heat}")
-//
-//
-//    val spices1 = listOf(
-//            Spice("curry", "mild"),
-//            Spice("pepper", "medium"),
-//            Spice("cayenne", "spicy"),
-//            Spice("ginger", "mild"),
-//            Spice("red curry", "medium"),
-//            Spice("green curry", "mild"),
-//            Spice("hot pepper", "extremely spicy")
-//    )
-//
-//    val spice = Spice("cayenne", spiciness = "spicy")
-//
-//    val spicelist = spices1.filter { it.heat < 5 }
-//
-//    fun makeSalt() = Spice("Salt")
-
     val curry = Curry("spicy yellow curry", "spicy")
     println(curry.color)
-}
 
-class SimpleSpice {
-    var name = "curry"
-    var spiciness = "mild"
-
-    var heat: Int
-        get() = when(spiciness) {"mild" -> 5 "fucking spicy" -> 9 else -> 0 }
-        set(value) { spiciness = when(value) { 5 -> "mild" 9 -> "fucking spicy" else -> "not spicy" } }
+    fun makeSpicyCurry() = Curry("Spicy curry", spiciness = "spicy")
 }
 
 abstract class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor): SpiceColor by color {
-
     var heat: Int
         get() {
             return when(spiciness) {
