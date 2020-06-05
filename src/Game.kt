c3f17668f6e8c60212e4f754d83522b249729d55
Quentin Désert <quentin.desert@gmail.com>
2020-06-05 13:13:03 -0500

    Inlined and practice (Game)

--- src/Game.kt
+++ src/Game.kt
@@ -1,28 +1,45 @@
-enum class Directions {
+enum class Direction {
     NORTH, SOUTH, EAST, WEST, START, END
 }
 
 class Game {
-    var path = mutableListOf(Directions.START)
-    val north = { path.add(Directions.NORTH) }
-    val south = { path.add(Directions.SOUTH) }
-    val east = { path.add(Directions.EAST) }
-    val west = { path.add(Directions.WEST) }
-    val end = {
-        path.add(Directions.END)
+    private var path = mutableListOf(Direction.START)
+    private val north = { path.move(Direction.NORTH) }
+    private val south = { path.move(Direction.SOUTH) }
+    private val east = { path.move(Direction.EAST) }
+    private val west = { path.move(Direction.WEST) }
+    private val end = {
+        path.add(Direction.END)
         println("Game Over: $path")
         path.clear()
         false
+    }
+
+    private fun MutableList<Direction>.move(direction: Direction): Boolean {
+        this.add(direction)
+        println("Moving $direction")
+        return true
+    }
+
+    private fun move(where: () -> Boolean) {
+        where.invoke()
+    }
+
+    fun makeMove(command: String?) {
+        when (command) {
+            "n" -> move(north)
+            "s" -> move(south)
+            "e" -> move(east)
+            "w" -> move(west)
+            else -> move(end)
+        }
     }
 }
 
 fun main() {
     val game = Game()
-    println(game.path)
-    game.north()
-    game.east()
-    game.south()
-    game.west()
-    game.end()
-    println(game.path)
+    while (true) {
+        print("Enter a direction: n/s/e/w:")
+        game.makeMove(readLine())
+    }
 }
--- src/aquarium5/Aquarium5.kt
+++ src/aquarium5/Aquarium5.kt
@@ -12,12 +12,24 @@ fun fishExamples() {
     val fish = Fish("splashy")
 
     with (fish.name) {
-        println(capitalize())
+        capitalize()
     }
+
     // equivalent to
     myWith(fish.name) {
-        println(capitalize())
+        capitalize()
     }
+    // and this is also equivalent to
+    myWith(fish.name, object: Function1<String, Unit> {
+        override fun invoke(name: String) {
+            name.capitalize()
+        }
+    }) // the lambda expression is an instance of function interface, subtype of object
+    // every time myWith is called an instance of the function is created.
+    // To solve this, Kotlin let's us define myWith as inline: See function definition below
+
+    // with inline, no object is created, and lambda body is inlined here
+    fish.name.capitalize()
 
     println(fish.run { name }) // run returns the result of the block function
 
@@ -31,7 +43,7 @@ fun fishExamples() {
             .let { it.length }
             .let { it + 31 })
 
-    // Returns only numbers that are divisible by 3 using higher-order function
+    // Practice (higher-order functions): Returns only numbers that are divisible by 3 using higher-order function
     val numbers = listOf<Int>(1,2,3,4,5,6,7,8,9,0)
     print(numbers.filterBy { isDivisibleBy3(it) })
 }
@@ -39,6 +51,8 @@ fun fishExamples() {
 fun myWith(name: String, block: String.() -> Unit) {
     name.block()
 }
+
+// Practice (higher-order functions)
 
 fun List<Int>.filterBy( block: (Int) -> Boolean ): List<Int> {
     val result = mutableListOf<Int>()
@@ -49,3 +63,9 @@ fun List<Int>.filterBy( block: (Int) -> Boolean ): List<Int> {
 }
 
 fun isDivisibleBy3(number: Int) = number.rem(3) == 0
+
+// inline version of myWith
+inline fun myWithInline(name: String, block: String.() -> Unit) {
+    name.block()
+} // Every time myWithInline is called, it will transform the source code to inline - the function.
+// Note: inlining large function does increase code size, best used for simple functions like myWith.
